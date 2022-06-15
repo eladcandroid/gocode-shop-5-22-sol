2c45203cb471bb934110e7faf788a262720558e1
Elad Cohen <eladc.android@gmail.com>
2022-06-15 10:47:57 +0300

    Add shopping cart

--- src/App.js
+++ src/App.js
@@ -1,63 +1,34 @@
-import { useState } from "react";
-import { useEffect } from "react";
 import "./App.css";
-import Header from "./components/Header/Header.js";
-import Products from "./components/Products/Products.js";
-import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner.js";
+import React, { useEffect, useState, useContext } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import ProductDetails from "./Views/ProductDetails";
+import About from "./Views/About";
+import Home from "./Views/Home";
+// import FromApp from "./contexts/FromApp"
 
 function App() {
-  const [isError, setIsError] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-  const [category, setCategory] = useState("all");
-  const [products, setProducts] = useState([]);
+  // const [CartArray, setCartArray] = useState([]);
+  // const [fixedArray, setFixedArray] = useState([]);
 
-  const fetchProducts = () => {
-    setIsLoading(true);
-    fetch("https://fakestoreapi.com/products")
-      .then((response) => response.json())
-      .then((products) => {
-        setProducts(products);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        setIsLoading(false);
-        setIsError(true);
-      });
-  };
-
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-
-  const categories = products
-    .map((product) => product.category)
-    .filter(
-      (category, index, categories) => categories.indexOf(category) === index
-    )
-    .sort();
+  // function addToCart(id) {
+  //   let increasingCartArray = [
+  //     ...CartArray,
+  //     fixedArray.find(function (item) {
+  //       return item.id === id;
+  //     }),
+  //   ];
+  //   setCartArray(increasingCartArray);
+  // }
 
   return (
-    <div>
-      {isLoading ? (
-        <LoadingSpinner />
-      ) : isError ? (
-        <div>The server isn't responding. Please try again later.</div>
-      ) : (
-        <>
-          <Header
-            setCategory={setCategory}
-            categories={categories}
-            fetchProducts={fetchProducts}
-          />
-          <Products
-            collection={products.filter((product) =>
-              category === "all" ? true : product.category === category
-            )}
-          />
-        </>
-      )}
-    </div>
+    // <FromApp.Provider value={{addToCart:addToCart}}>
+
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/Products/:dynamicProductID" element={<ProductDetails />} />
+      <Route path="about" element={<About />} />
+    </Routes>
+    // </FromApp.Provider>
   );
 }
-
 export default App;
